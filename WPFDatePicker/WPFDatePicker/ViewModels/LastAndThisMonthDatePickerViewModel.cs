3f45e1e62ced085137d2cf463dfd6862bf572a03
Tetsuo Honda <t-honda@hondarer-soft.com>
2021-06-17 07:21:02 +0900

    うつわを作成

---
+++ WPFDatePicker/WPFDatePicker/ViewModels/LastAndThisMonthDatePickerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace WPFDatePicker.ViewModels
+{
+    public class LastAndThisMonthDatePickerViewModel : BindableBase
+    {
+        private DateTime _selectedDate = DateTime.Today;
+
+        public string SelectedDate
+        {
+            get
+            {
+                return string.Format("{0:yyyy/MM/dd(ddd)}",_selectedDate);
+            }
+            set
+            {
+                // 無効な値の場合にはソースを更新しない
+
+                // 曜日を取り除く(日付だけ変更することを考慮)
+                string targetValue = Regex.Replace(value, @"\(.*\)", string.Empty);
+
+                // 日付として評価する
+                //if (!DateTime.TryParseExact(targetValue, "y/M/d", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime parsedDateTime))
+                if (!DateTime.TryParse(targetValue, out DateTime parsedDateTime)) // 現在のカルチャで解釈可能なら良いとする
+                {
+                    OnPropertyChanged();
+                    return;
+                }
+
+                // TODO: 選択可能な範囲になければクリップ
+
+                SetProperty(ref _selectedDate, parsedDateTime);
+            }
+        }
+    }
+}
---
+++ WPFDatePicker/WPFDatePicker/Views/LastAndThisMonthDatePicker.cs
@@ -0,0 +1,58 @@
+﻿// 以下を処理すれば、左右キーで選択範囲を変更したり、上下キーで値を変更したりできるはず
+// TextBox.Select(Int32, Int32)
+// TextBox.SelectionStart
+// TextBox.SelectionLength
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace WPFDatePicker.Views
+{
+    /// <summary>
+    /// 前月と今月に特化した日付選択部品を提供します。
+    /// </summary>
+    [TemplatePart(Name = "PART_dateTextBox", Type = typeof(TextBox))]
+    public class LastAndThisMonthDatePicker : Control
+    {
+        /// <summary>
+        /// <see cref="LastAndThisMonthDatePicker"/> クラスの静的な初期化をします。
+        /// </summary>
+        static LastAndThisMonthDatePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LastAndThisMonthDatePicker), new FrameworkPropertyMetadata(typeof(LastAndThisMonthDatePicker)));
+        }
+
+        public LastAndThisMonthDatePicker()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void OnApplyTemplate()
+        {
+            if (Template.FindName("PART_dateTextBox", this) is TextBox dateTextBox)
+            {
+                // フォーカスを得たときに全選択する
+                dateTextBox.GotKeyboardFocus += (sender, e) =>
+                {
+                    ((TextBox)sender).SelectAll();
+                };
+
+                // Enter キーを入力した際に、ソースを更新する
+                dateTextBox.KeyDown += (sender, e) =>
+                {
+                    if (e.Key != Key.Enter)
+                    { 
+                        return;
+                    }
+                    BindingExpression be = ((TextBox)sender).GetBindingExpression(TextBox.TextProperty);
+                    be.UpdateSource();
+                    e.Handled = true;
+                };
+            }
+
+            base.OnApplyTemplate();
+        }
+    }
+}
